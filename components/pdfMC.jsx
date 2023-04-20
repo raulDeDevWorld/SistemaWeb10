1f46d7d014cb4ef9e6ad3338a18c39894c722ff8
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-20 08:09:53 -0400

    Update pdfMC.jsx
    Cc

--- components/pdfMC.jsx
+++ components/pdfMC.jsx
@@ -426,17 +426,17 @@ const PDFView = ({ click }) => {
 
                         <View style={styles.viewKeyValueTwoYellow} >
                             <Text style={styles.valueElaborador}></Text>
-                            <Text style={styles.valueElaborador}>ACEPTACION DE SERVICIO:</Text>
+                            <Text style={styles.valueElaborador}>FIRMA Y SELLO:</Text>
                             <Text style={styles.valueElaborador}></Text>
                         </View>
-                        <View style={styles.viewKeyValueTwo}>
+                        <View style={styles.viewKeyValueTwoYellow}>
                             <Text style={styles.valueElaborador}></Text>
                             <Text style={styles.valueElaborador}>NOMBRE:</Text>
                             <Text style={styles.valueElaboradorLine}></Text>
                         </View>
                         <View style={styles.viewKeyValueTwo} >
                             <Text style={styles.valueElaborador}></Text>
-                            <Text style={styles.valueElaborador}>CI:</Text>
+                            <Text style={styles.valueElaborador}>FECHA:</Text>
                             <Text style={styles.valueElaborador}></Text>
                         </View>
 
